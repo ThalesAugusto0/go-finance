f88f8e71be8379a8ef166ab203907f1900504870
ThalesAugusto0 <thalllesaugustos28@gmail.com>
2023-03-25 13:36:25 -0300

    feat: criada a lista com os gastos e feito estilizacao
    Feita lista usando o flatlist, para dar o efeito de movimento, adicionado o 'contentContainerStyle'
    para dar um espaço do bottom do celular

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HighlightCard } from '../../components/HighlightCard/index';
+import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
 import {
     Container, 
     Header, 
@@ -10,10 +11,49 @@ import {
     UserName,
     UserWrapper,
     Icon,
-    HighlightCards
+    HighlightCards,
+    Transactions,
+    Title,
+    TransactionList
 } from './styles'
 
+export interface DataListProps extends TransactionCardProps {
+    id: string;
+}
+
 export function Dashboard() {
+    const data: DataListProps[] = [
+        {
+            id: '1',
+            type: 'positive',
+            title: 'Desenvolvimento de Site',
+            amount: 'R$ 12.000,00',
+            category: {
+                name: 'Vendas', 
+                icon: 'dollar-sign'},
+            date: '14/12/2020'
+        },
+        {
+            id: '2',
+            type: 'negative',
+            title: 'Hamburgueria Pizzy',
+            amount: 'R$ 59,00',
+            category: {
+                name: 'Alimentação', 
+                icon: 'coffee'},
+            date: '14/12/2020'            
+        },
+        {
+            id: '3',
+            type: 'negative',
+            title: 'Aluguel do Apartamento',
+            amount: 'R$ 1.200,00',
+            category: {
+                name: 'Casa', 
+                icon: 'shopping-bag'},
+            date: '14/12/2020'            
+        }
+    ];
     return(
         <Container>
             <Header>
@@ -49,6 +89,17 @@ export function Dashboard() {
                     amount='R$ 16.141,00' 
                     lastTransaction='01 à 16 de abril'/>
             </HighlightCards>
+
+            <Transactions>
+                <Title>Listagem</Title>
+
+                <TransactionList 
+                    data={data}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => <TransactionCard data={item}/>}
+                />
+
+            </Transactions>
         </Container>
     )
 }
