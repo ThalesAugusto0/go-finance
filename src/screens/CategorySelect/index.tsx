624c94097b8fbc5154ac66bff57308dc6244110e
ThalesAugusto0 <thalllesaugustos28@gmail.com>
2023-03-27 22:34:08 -0300

    feat: criado a primeira parte da tela de cadastro, amanha termino o restante

---
+++ src/screens/CategorySelect/index.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { FlatList } from "react-native";
+import { categories } from "../../utils/categories";
+import { Button } from "../../components/Form/Button";
+import { 
+    Container,
+    Header,
+    Title,
+    Catergory,
+    Icon,
+    Name,
+    Separator,
+    Footer,
+ } from "./styles";
+
+interface Category{
+    key: string;
+    name: string;
+}
+
+interface Props {
+    category: Category;
+    setCategory: (item: string) => void;
+    closeSelectCategory: () => void;
+}
+
+export function CategorySelect({
+    category,
+    setCategory,
+    closeSelectCategory
+}: Props) {
+
+    function handleCategorySelect(category: Category){
+        setCategory(category);
+    }
+
+    return(
+        <Container>
+            <Header>
+                <Title>Categoria</Title>
+            </Header>
+            <FlatList 
+                data={categories}
+                style={{ flex: 1, width: '100%' }}
+                keyExtractor={(item) => item.key}
+                renderItem={({ item }) => (
+                    <Catergory
+                        onPress={() => handleCategorySelect(item)}
+                        isActive={category}
+                    >
+                        <Icon name={item.icon}/>
+                        <Name>{item.name}</Name>
+                    </Catergory>
+                )}
+                ItemSeparatorComponent={() => <Separator />}
+            />
+
+            <Footer>
+                <Button
+                    onPress ={closeSelectCategory} 
+                    title="Selecionar"
+                />
+            </Footer>
+        </Container>
+    )
+}
