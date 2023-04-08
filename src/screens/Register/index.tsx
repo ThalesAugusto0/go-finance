d844e3a6edd5aec49e2a6f0c92fccf567f672f27
ThalesAugusto0 <thalllesaugustos28@gmail.com>
2023-04-08 16:52:44 -0300

    feat: criado o AsyncStorage
    Criado código para salvar os dados no dispisitivo do user, criado a recuperação dos dados salvos e
    criado o código para remover os dados do mobile do user.

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Modal, TouchableWithoutFeedback, Keyboard, Alert } from "react-native";
 import { Control, FieldValues, useForm } from "react-hook-form";
 import * as Yup from "yup";
@@ -11,11 +11,13 @@ import {
   Fields,
   TransactionsTypes,
 } from "./styles";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { InputForm } from "../../components/Form/InputForm";
 import { Button } from "../../components/Form/Button";
 import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton";
 import { CategorySelectButton } from "../../components/Form/CategorySelectButton";
 import { CategorySelect } from "../CategorySelect";
+import uuid from "react-native-uuid";
 
 interface FormData {
   [name: string]: string;
@@ -31,6 +33,8 @@ const schema = Yup.object().shape({
 export function Register() {
   const [transactionType, setTransactionType] = useState("");
   const [categoryModalOpen, setCategoryModalOpen] = useState(false);
+
+  const dataKey = "@gofinances:transactions";
 
   const [category, setCategory] = useState({
     key: "category",
@@ -59,20 +63,52 @@ export function Register() {
     setCategoryModalOpen(false);
   }
 
-  function handleRegister(form: FormData) {
+  async function handleRegister(form: FormData) {
     if (!transactionType) return Alert.alert("Selecione o tipo da transação");
 
     if (category.key === "category")
       return Alert.alert("Selecione a categoria");
 
-    const data = {
+    const newTransaction = {
+      id: String(uuid.v4()),
       name: form.name,
       amount: form.amount,
       transactionType,
       category: category.key,
+      date: new Date(),
     };
-    console.log(data);
+
+    try {
+      const data = await AsyncStorage.getItem(dataKey);
+
+      const currentData = data ? JSON.parse(data) : [];
+
+      const dataFormatted = [...currentData, newTransaction];
+
+      await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormatted));
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Não foi possível salvar");
+    }
   }
+  /*
+   * 1.Criado código para salvar os dados no dispositivo do user.
+   * 2.Criado a recuperação dos dados salvos.
+   * 3.Criado código para remover dados do mobile do user.
+   */
+  useEffect(() => {
+    async function loadData() {
+      const data = await AsyncStorage.getItem(dataKey);
+      console.log(JSON.parse(data!));
+    }
+    loadData();
+
+    /* Remove o que foi salvo no dispositivo do usuário.
+    async function removeAll() {
+      await AsyncStorage.removeItem(dataKey);
+    }
+    removeAll();*/
+  }, []);
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
